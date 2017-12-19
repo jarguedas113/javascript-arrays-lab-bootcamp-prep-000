b91066a3bbf5e6833679c0dcd1aa0732b8338e7a
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-19 18:37:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function kittens() {
+return kittens()
+}
+
+function destructivelyAppendKitten(name) {
+kittens.push('Ralph');
+return destructivelyAppendKitten
+}
+
+
+function destructivelyPrependKitten(name) {
+kittens.unshift("Bob")
+return destructivelyPrependKitten
+}
+
+function destructivelyRemoveLastKitten() {
+kittens.pop(0, kittens.length - 1)
+return destructivelyRemoveLastKitten
+}
+
+function destructivelyRemoveFirstKitten() {
+kittens.pop(-0)
+return destructivelyRemoveFirstKitten
+}
